a3194c761a35b5244ae6e6656315161fd7b9b159
KjOHnBaggins <okenshieldak47@gmail.com>
2023-01-06 18:16:23 +0630

    configure store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -32,6 +32,10 @@ const store = configureStore({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
